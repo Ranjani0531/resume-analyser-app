a012918e9a9df5b8af9606858a20cfffa46e9e74
Ranjani0531 <ranjani992022@gmail.com>
2026-06-20 21:56:41 +0530

    myinfra please work godddd pls

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -9,24 +9,9 @@ resource "aws_instance" "webserver" {
 }
 
 
-resource "aws_instance" "Dbsever" {
-  
-  ami = "ami-02777684819ca2214"
-  instance_type = "t3.micro"
-  key_name = "resume-analyser"
-  subnet_id = aws_subnet.Resume_private_subnet.id
-  vpc_security_group_ids = [aws_security_group.nsg2.id]
-}
 
 output "webserver_public_ip" {
   value = aws_instance.webserver.public_ip
-}
-
-output "Dbserver_private_ip" {
-  value = aws_instance.Dbsever.private_ip
-}
-output "Dbserver_public_ip" {
-  value = aws_instance.Dbsever.public_ip
 }
 
 output "webserver_private_ip" {
