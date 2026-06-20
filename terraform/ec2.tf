743e71b09582118fecd79553c20c882a15359e36
Ranjani0531 <ranjani992022@gmail.com>
2026-06-20 23:36:05 +0530

    murugaa

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -8,12 +8,6 @@ resource "aws_instance" "webserver" {
   vpc_security_group_ids = [aws_security_group.nsg1.id]
 }
 
-
-
 output "webserver_public_ip" {
   value = aws_instance.webserver.public_ip
-}
-
-output "webserver_private_ip" {
-  value = aws_instance.webserver.private_ip
 }
