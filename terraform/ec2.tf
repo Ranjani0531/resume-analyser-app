c0aaed83988e3586f3b39ab05b4bdc1926130918
Ranjani0531 <ranjani992022@gmail.com>
2026-06-21 21:51:49 +0530

    aws eip enable

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -7,6 +7,16 @@ resource "aws_instance" "webserver" {
   associate_public_ip_address = "true"
   vpc_security_group_ids = [aws_security_group.nsg1.id]
 }
+resource "aws_eip" "web_ip" {
+  domain = "vpc"
+}
+
+resource "aws_eip_association" "web_ip_associate" {
+  instance_id = aws_instance.webserver.id
+  allocation_id = aws_eip.web_ip.id
+}
+
+
 
 output "webserver_public_ip" {
   value = aws_instance.webserver.public_ip
